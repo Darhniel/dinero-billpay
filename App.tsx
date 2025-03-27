9303a58c3a18dfaa9cda002ca74bcbc05e1dea4e
Michael Ilesanmi <ioluwamichael@gmail.com>
2025-03-27 01:04:34 +0100

    Refactor App.tsx to import React and add expo-localization plugin in app.json

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import { StyleSheet, Text, View } from 'react-native';
 import StackNavigator from './navigation/StackNavigator';
 import * as SplashScreen from 'expo-splash-screen';
 import { useFonts } from 'expo-font';
-import { useEffect } from 'react';
+import React, { useEffect } from 'react';
 
 SplashScreen.preventAutoHideAsync();
 
